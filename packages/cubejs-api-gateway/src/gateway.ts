dd37511e9396efe6efdc56bf77d512bb199518d5
buu.nguyen <buu.nguyen@cake.vn>
2025-12-10 11:01:15 +0700

    feat(api-gateway): add requestId to transform data parameters

--- packages/cubejs-api-gateway/src/gateway.ts
+++ packages/cubejs-api-gateway/src/gateway.ts
@@ -1871,6 +1871,7 @@ class ApiGateway {
       external: response.external,
       slowQuery: Boolean(response.slowQuery),
       total: normalizedQuery.total ? response.total : null,
+      requestId: context.requestId,
     };
 
     resultWrapper.setTransformData(transformDataParams);
